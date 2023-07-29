cc74501bbfc939cd01029ce92c087ec3db7e2422
Irene Buck <irenedevito@gmail.com>
2023-07-28 20:15:53 -0700

    Updated for shift above 25, reuse, and artwork

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -1,42 +1,35 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
+from art import logo
+print(logo)
 
 # Ceaser cipher encrypts or decrpyts user text.  https://en.wikipedia.org/wiki/Caesar_cipher
-def ceasar(e_or_d, user_text, shift_num):
-    ciphered = ""
-    if e_or_d == "encode":
-        for letter in user_text:
-            index = alphabet.index(letter)
-            new_index = index + shift_num
-            new_letter = alphabet[new_index]
-            ciphered += new_letter
-        print(f'The encoded text is {ciphered}.')
-    else:
-        for letter in user_text:
-            index = alphabet.index(letter)
-            new_index = index - shift_num
-            new_letter = alphabet[new_index]
-            ciphered += new_letter
-        print(f'The decoded text is {ciphered}.')
-
-
-ceasar(e_or_d=direction, user_text=text, shift_num=shift)
-
-
-# Another way to write this in a more concise way
+# Only letters are encoded/decoded
 def caesar(start_text, shift_amount, cipher_direction):
     output = ""
     if cipher_direction == "decode":
         shift_amount *= -1     # this allows the shifting to decode in line 37, subtracting value
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        output += alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            output += alphabet[new_position]
+        else:
+            output += char     # captures the non-alphabet values and spaces
     print(f"Here's the {direction}d result: {output}.")
 
+go_again = True
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+while go_again:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    result = input("Type 'yes' if you want to go again. Otherwise, type 'no'.")
+
+    if result == 'no':
+        go_again = False
+        print("Goodbye!")
